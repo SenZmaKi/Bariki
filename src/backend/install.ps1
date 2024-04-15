16ca165d3f3ad2f431fc630138c16449c2812bc1
Sen ZmaKi <phidelalvin@gmail.com>
2024-04-15 22:46:49 +0300

    Update install script

--- src/backend/install.ps1
+++ src/backend/install.ps1
@@ -1,27 +1,123 @@
-echo "Verifying python version"
-$pythonVersion = python --version
-$pythonVersion = $pythonVersion.Replace("Python ", "")
-$majorVersion = $pythonVersion.Split(".")[0]
-$minorVersion = $pythonVersion.Split(".")[1]
+function CommandExists {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$command,
+        [Parameter(Mandatory=$false)]
+        [bool]$exitOnError = $false
+    )
 
-New-Item -ItemType Directory -Force -Path Bariki/src/backend
-
-if ($majorVersion -lt 3 -or ($majorVersion -eq 3 -and $minorVersion -lt 11)) {
-    Write-Output "You need python 3.11 or greater"
-    exit 1
+    $commandTool = $command.Split(" ", 2)[0]
+    try {
+        Get-Command $commandTool -ErrorAction Stop | Out-Null
+        if (-not $exitOnError) {
+            return $true
+          }
+    } catch {
+        if ($exitOnError) {
+            Write-Output "Command '$commandTool' does not exist. Exiting."
+            exit 1
+        }
+        return $false
+    }
 }
-echo "Installing git"
-winget install git
-echo "Cloning repo"
-git clone https://github.com/SenZmaKi/Bariki.git
 
-echo "Installing docker"
-winget install Docker.DockerDesktop
-echo "Installing ubuntu"
-winget install Canonical.Ubuntu.2204 
+function RunAndExitOnFailure {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$command
+    )
+    CommandExists $command $true
+    Invoke-Expression $command
+    if ($LASTEXITCODE -ne 0) {
+        WriteRed "Command '$command' failed with exit code $LASTEXITCODE. Exiting."
+        exit $LASTEXITCODE
+    }
+}
 
-echo "Installing dependencies"
-cd src/backend
-python -m venv .venv
-./.venv/Scripts/activate.bat
-pip install -r requirements.txt
+function MakeWingetCommand {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$package
+  )
+
+  return "winget install $package --silent --accept-package-agreements --accept-source-agreements"
+}
+
+function WriteGreen {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Text
+    )
+    Write-Host $Text -ForegroundColor Green
+}
+
+function WriteRed {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Text
+    )
+    Write-Host $Text -ForegroundColor Red
+}
+
+function CheckAndInstallTool {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$toolName,
+        [Parameter(Mandatory=$true)]
+        [string]$installCommand
+    )
+
+    if (CommandExists $toolName) {
+        WriteGreen "$toolName is already installed."
+        return
+      }
+    WriteRed "$toolName is not installed."
+    WriteGreen "Installing $toolName..."
+    RunAndExitOnFailure $installCommand
+    WriteGreen "Installed $toolName "
+
+}
+
+function CheckPythonVersion {
+    CommandExists $pyCommand $true
+    try {
+
+        $pythonVersion = Invoke-Expression "$pyCommand --version"
+        if ($LASTEXITCODE -ne 0) {
+            throw
+          }
+    } catch {
+        WriteRed "Python 3.12 is not installed"
+        WriteGreen "Installing Python 3.12..."
+        RunAndExitOnFailure (MakeWingetCommand "Python.Python.3.12")
+        WriteGreen "Installed Python 3.12"
+    }
+}
+
+
+WriteGreen "Starting project setup.. ."
+
+$pyCommand = "py -3.12"
+WriteGreen "Verifying python version"
+CheckPythonVersion
+
+
+CheckAndInstallTool "git" (MakeWingetCommand "Git.Git")
+WriteGreen "Cloning repo"
+RunAndExitOnFailure "git clone https://github.com/SenZmaKi/Bariki.git"
+
+Set-Location Bariki
+
+CheckAndInstallTool "docker" (MakeWingetCommand "Docker.DockerDesktop")
+
+CheckAndInstallTool "ubuntu" (MakeWingetCommand "Canonical.Ubuntu.2204")
+
+WriteGreen "Installing dependencies"
+Set-Location src/backend
+RunAndExitOnFailure "$pyCommand -m venv .venv"
+RunAndExitOnFailure "./.venv/Scripts/activate.bat"
+RunAndExitOnFailure "$pyCommand -m pip install -r requirements.txt"
+
+Write-Host "If you're having issues with ubuntu or docker make sure to enable virtualisation in your BIOS settings: https://support.microsoft.com/en-us/windows/enable-virtualization-on-windows-11-pcs-c5578302-6e43-4b4b-a449-8ced115f58e1"
+Write-Host "If you're having issues with algokit try: https://stackoverflow.com/a/66409838/17193072 "
+WriteGreen "Successfully setup project"
