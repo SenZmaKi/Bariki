d3d1314f04014698bec0f83bb986ce158219c8d2
Kennedylelei <kennedylelei26@gmail.com>
2024-04-16 14:49:29 +0300

    route: create_user

--- src/backend/app/app.py
+++ src/backend/app/app.py
@@ -4,9 +4,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
     """ Index page """
     return "<h1>Index </h1>"
 
-app.route('/create_user'/)
+@app.route('/create_user/')
+def create_user():
+    """ Create user route """
+    return "<h1>Create User</h1>"
