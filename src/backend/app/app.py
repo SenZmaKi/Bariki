66cd15ca0990a2c0b2d28d8676a530d7ab30610e
omwami <ianalpha@protonmail.com>
2024-04-16 12:57:55 +0300

    created index app file

---
+++ src/backend/app/app.py
@@ -0,0 +1,10 @@
+""" App main module """
+from flask import Flask, redirect, request
+from flask import render_template
+
+app = Flask(__name__)
+
+app.route("/")
+def index():
+    """ Index page """
+    return "<h1>Index </h1>"
---
+++ src/backend/app/models.py
@@ -0,0 +1,2 @@
+""" Module with database models"""
+
