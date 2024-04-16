bac4dfe9701253a32b6e5f1b7809d849ceb8792f
Sen ZmaKi <phidelalvin@gmail.com>
2024-04-17 02:20:53 +0300

    Test and Finish algo module, kill me please

--- src/backend/algo/api.py
+++ src/backend/algo/api.py
@@ -1,33 +1,23 @@
 from dataclasses import dataclass
-from typing import cast
+from typing import Any, cast
 from algosdk import account, transaction
 from algokit_utils import get_dispenser_account
+from algosdk.constants import MIN_TXN_FEE
 from beaker import localnet
-from beaker.client import ApplicationClient
-from .contract import app
-from . import contract
 
 ALGOD_CLIENT = localnet.get_algod_client()
 INDEXER_CLIENT = localnet.get_indexer_client()
 DISPENSER_ACCOUNT = get_dispenser_account(ALGOD_CLIENT)
-app.build().export("./artifacts")
+MIN_ACC_BALANCE = 100_000
+TRANSACTION_FEE = MIN_TXN_FEE
+"""Algorand rejects any transactions that results in the account balance being below this"""
 
 
 @dataclass
 class Donation:
-    donor_address: str
-    smart_contract_address: str
+    donor_account_address: str
+    account_cause_address: str
     amount: int
-
-
-def _get_app_client(account_address: str) -> ApplicationClient:
-    acc = _get_account(account_address)
-    return ApplicationClient(
-        client=ALGOD_CLIENT,
-        app=app,
-        sender=acc.address,
-        signer=acc.signer,
-    )
 
 
 def _get_account(account_address: str) -> localnet.LocalAccount:
@@ -36,12 +26,6 @@ def _get_account(account_address: str) -> localnet.LocalAccount:
         if acc.address == account_address:
             return acc
     raise ValueError(f"Failed to find {account_address} in localnet")
-
-
-def _create_new_account() -> str:
-    private_key, account_address = account.generate_account()
-    localnet.add_account(private_key)
-    return account_address
 
 
 def _make_transaction(
@@ -62,108 +46,97 @@ def _make_transaction(
     transaction.wait_for_confirmation(ALGOD_CLIENT, txid)
 
 
-def create_new_cause_account(goal_amount: int) -> str:
+def donate(amt: int, donor_account_address: str, cause_account_address: str) -> None:
     """
-    Create a new account for a cause
-    :returns: The account address
+    Donate into the cause account address
     """
-    _, account_address = _create_new_account()
-    app_client = cast(ApplicationClient, _get_app_client(account_address))
-    app_client.create()
-    app_client.opt_in()
-    app_client.call(contract.set_goal_amount, amt=goal_amount)
-    return account_address
-
-
-def create_new_donor_account() -> str:
-    """
-    Create a new account for a donator
-    :returns: The account address
-    """
-    account_address = _create_new_account()
-    return account_address
-
-
-def get_current_amount(smart_contract_address: str) -> int:
-    """
-    Get the current amount that the cause has raised
-    """
-    app_client = _get_app_client(smart_contract_address)
-    info = app_client.get_application_account_info()
-    balance = info["amount"]
-    current_amount = balance - info.get("min_balance")
-    return current_amount
-
-
-def get_goal_amount(smart_contract_address: str) -> int:
-    """
-    Get the target goal amount for the cause
-    """
-    app_client = _get_app_client(smart_contract_address)
-    goal_amount = app_client.call(contract.get_goal_amount).return_value
-    return goal_amount
-
-
-def donate(amt: int, donor_account_address: str, smart_contract_address: str) -> bool:
-    """
-    Donate into the smart contract account
-    :returns: Boolean indicating whether this donation achieved the goal amount i.e., if `True`, goal reached else not reached
-    """
-    app_client = _get_app_client(smart_contract_address)
     sender_acc = _get_account(donor_account_address)
-    goal_amt = get_goal_amount(smart_contract_address)
+    balance = get_balance(donor_account_address, True)
+    full_spend = amt + TRANSACTION_FEE
+    new_balance = balance - full_spend
+    if new_balance < MIN_ACC_BALANCE:
+        raise Exception(
+            f"""Transaction + Transaction fee results in a balance below the minimum account balance i.e., 
+            {amt} + {TRANSACTION_FEE} = {full_spend}
+            {balance} - {full_spend} = {new_balance}
+            {new_balance} < {MIN_ACC_BALANCE}"""
+        )
+
     _make_transaction(
         amt,
         sender_acc.private_key,
         sender_acc.address,
-        smart_contract_address,
-        sp=app_client.get_suggested_params(),
+        cause_account_address,
+        ALGOD_CLIENT.suggested_params(),
     )
-    current_amt = get_current_amount(smart_contract_address)
-    if current_amt >= goal_amt:
-        fund(smart_contract_address)
-        return True
-    return False
 
 
 def add_funds(amount: int, donor_account_address: str) -> None:
     """
     Load funds into the donor's account
     """
+    sp = ALGOD_CLIENT.suggested_params()
     _make_transaction(
         amount,
         DISPENSER_ACCOUNT.private_key,
         DISPENSER_ACCOUNT.address,
         donor_account_address,
-        ALGOD_CLIENT.suggested_params(),
+        sp=sp,
     )
 
 
-def get_balance(donor_account_address: str) -> int:
+def get_balance(account_address: str, get_real_balance=False) -> int:
     """
-    Get the donor's account balance
+    Get the account's balance
+    :param: get_real_balance: boolean indicating whether to not subtract minimum account balance from the real balance, leave as `False` if you don't understand
     """
-    info = INDEXER_CLIENT.account_info(donor_account_address)
-    balance = info["amt"] - info["min_balance"]
+    resp = cast(dict[str, Any], ALGOD_CLIENT.account_info(account_address))
+    real_balance = resp["amount"]
+    if get_real_balance:
+        return real_balance
+    balance = real_balance - MIN_ACC_BALANCE
     return balance
 
 
-def fund(smart_contract_address: str) -> None:
-    """
-    Release all funds currently held in the smart contract's account into the cause's account
-    - Note: This is automatically invoked during a donation when the donation reaches the goal amount
-    """
-    app_client = _get_app_client(smart_contract_address)
-    amt = get_current_amount(smart_contract_address)
-    app_client.fund(amt)
-
-
 def get_donations(donor_account_address: str) -> list[Donation]:
+    # FIXME: The indexer doesn't reflect tranasactions immediately after they happen for some reason
+    # so sometimes a recently processed transaction appears as if it never happened
     """
+    Warning don't use this shit it doesn't work
     Retrieve all donations made by the donor
     """
-    txns = INDEXER_CLIENT.search_transactions(
-        address=donor_account_address, address_role="sender"
-    )
-    donations = [Donation(txn["snd"], txn["rcx"], txn["amt"]) for txn in txns]
-    return donations
+
+    def helper(next_page: str | None = None):
+        result = INDEXER_CLIENT.search_transactions(
+            next_page=next_page,
+        )
+        txns = result["transactions"]
+        print(txns)
+        donations = [
+            Donation(
+                txn["sender"],
+                txn["payment-transaction"]["receiver"],
+                txn["payment-transaction"]["amount"],
+            )
+            for txn in txns
+            if txn["sender"] == donor_account_address
+        ]
+
+        return result.get("next-token", None), donations
+
+    next_page, accumulated_donations = helper()
+    while next_page is not None:
+        next_page, donations = helper(next_page)
+        accumulated_donations.extend(donations)
+    return accumulated_donations
+
+
+def create_account() -> str:
+    """
+    Create an account for a cause/donor
+    :return: account address
+    """
+    private_key, account_address = account.generate_account()
+    localnet.add_account(private_key)
+    add_funds(MIN_ACC_BALANCE, account_address)
+    return account_address
